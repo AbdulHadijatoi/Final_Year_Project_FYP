7df55fa85072309eb72ab1c0564ab969f167a9bc
H∆DI <abdulhadijatoi@gmail.com>
2021-08-13 13:32:52 +0500

    Added header and footer and also removed the left border from mobile menu with full height

---
+++ Website/assets/js/panel.js
@@ -0,0 +1,21 @@
+let navList = document.getElementById('navList');
+let mobileNavList = document.getElementById('mobileNavList');
+
+function setDisplay(elemId, display) {
+    var x = document.getElementById(elemId);
+    x.style.display = display;
+}
+
+function toggleDisplay(thisElem, targetElemId) {
+    if (thisElem.checked) {
+        setDisplay(targetElemId, "block");
+    } else {
+        setDisplay(targetElemId, "none");
+    }
+}
+
+function clickTarget(targetId) {
+    document.getElementById(targetId).click();
+}
+
+mobileNavList.innerHTML = navList.innerHTML;
